d8e0fe5d33d74b9e4226e867d127a28dc143e4d8
Charith Ellawala <charithe@users.noreply.github.com>
2024-01-04 10:41:59 +0000

    chore: Update otelconnect-go to main (#137)
    * chore: Update otelconnect-go to main
    
    Needed to get the fix for metrics descriptions that clash with gRPC
    metrics.
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>
    
    * Update copyright header
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>
    
    ---------
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- bundle/client.go
+++ bundle/client.go
@@ -1,4 +1,4 @@
-// Copyright 2021-2023 Zenauth Ltd.
+// Copyright 2021-2024 Zenauth Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
 package bundle
@@ -122,12 +122,17 @@ func NewClient(conf ClientConf) (*Client, error) {
 		return nil, err
 	}
 
+	otelConnect, err := otelconnect.NewInterceptor()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create otel interceptor: %w", err)
+	}
+
 	httpClient := mkHTTPClient(conf) // Bidi streams don't work with retryable HTTP client.
 	retryableHTTPClient := mkRetryableHTTPClient(conf)
 	options := []connect.ClientOption{
 		connect.WithCompressMinBytes(1024),
 		connect.WithInterceptors(
-			otelconnect.NewInterceptor(otelconnect.WithoutMetrics()),
+			otelConnect,
 			newUserAgentInterceptor(),
 		),
 	}
