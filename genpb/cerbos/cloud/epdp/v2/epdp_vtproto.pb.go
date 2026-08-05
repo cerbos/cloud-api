27552c6ba2c23c803ea64f83251719fe2b916d2d
Andrew Haines <haines@cerbos.dev>
2026-08-05 11:54:23 +0100

    Add `strict_evaluation` to `cerbos.cloud.epdp.v2.Config.Evaluator` (#437)
    Signed-off-by: Andrew Haines <haines@cerbos.dev>

--- genpb/cerbos/cloud/epdp/v2/epdp_vtproto.pb.go
+++ genpb/cerbos/cloud/epdp/v2/epdp_vtproto.pb.go
@@ -58,6 +58,16 @@ func (m *Config_Evaluator) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
+	if m.StrictEvaluation {
+		i--
+		if m.StrictEvaluation {
+			dAtA[i] = 1
+		} else {
+			dAtA[i] = 0
+		}
+		i--
+		dAtA[i] = 0x28
+	}
 	if len(m.DefaultScope) > 0 {
 		i -= len(m.DefaultScope)
 		copy(dAtA[i:], m.DefaultScope)
@@ -887,6 +897,9 @@ func (m *Config_Evaluator) SizeVT() (n int) {
 	if l > 0 {
 		n += 1 + l + protohelpers.SizeOfVarint(uint64(l))
 	}
+	if m.StrictEvaluation {
+		n += 2
+	}
 	n += len(m.unknownFields)
 	return n
 }
@@ -1425,6 +1438,26 @@ func (m *Config_Evaluator) UnmarshalVT(dAtA []byte) error {
 			}
 			m.DefaultScope = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
+		case 5:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field StrictEvaluation", wireType)
+			}
+			var v int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				v |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			m.StrictEvaluation = bool(v != 0)
 		default:
 			iNdEx = preIndex
 			skippy, err := protohelpers.Skip(dAtA[iNdEx:])
